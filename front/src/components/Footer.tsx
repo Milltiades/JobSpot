9a3dd304e536998fd013ffd3ec113adff177f369
Milltiades <gigakakulia2017@gmail.com>
2023-06-30 11:21:28 +0400

    updated footer

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className=" gap-20 w-full bg-gray-950 text-white h-auto flex flex-col relative top-20 p-5 lg:px-40 lg:py-10">
+    <div className=" gap-20 w-full bg-gray-950 text-white h-auto flex flex-col  p-5 lg:px-40 lg:py-10">
       <h1 className=" font-bold text-3xl">JobSpot</h1>
       <div className="pt-2 border-t-2 opacity-50">
         <h3>
